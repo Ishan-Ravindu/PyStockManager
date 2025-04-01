5861adcda621a6b4467f06a0f1f532e174447e55
Ishan-Ravindu <ishanravindu27596@gmail.com>
2025-04-01 11:24:45 +0000

    refactor: project clean

--- inventory/admin/sales.py
+++ inventory/admin/sales.py
@@ -9,7 +9,7 @@ class SalesInvoiceItemInline(admin.TabularInline):
 
 @admin.register(SalesInvoice)
 class SalesInvoiceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'shop', 'customer', 'total_amount', 'created_at')
+    list_display = ('id', 'shop', 'customer', 'total_amount', 'paid_amount', 'created_at')
     list_filter = ('shop', 'customer', 'created_at')
     search_fields = ('shop__name', 'customer__name')
     readonly_fields = ('total_amount', 'created_at')
