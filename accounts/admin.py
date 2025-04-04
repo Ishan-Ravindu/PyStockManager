5a17edca2e5e1ef5638b37409ab82db73b816800
Ishan-Ravindu <ishanravindu27596@gmail.com>
2025-04-04 19:20:50 +0000

    fix: added account history iside account

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import Account, Withdraw, AccountTransfer, AccountTransactionHistory
 
+class AccountTransactionHistoryInLine(admin.TabularInline):
+    model = AccountTransactionHistory
+    extra = 0
+    can_delete = False    
+    fields = ('transaction_type', 'amount', 'previous_balance', 'new_balance', 'timestamp', 'description')
+    
+    def get_readonly_fields(self, request, obj=None):
+        return self.fields
+    
+    def has_add_permission(self, request, obj=None):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'balance')
     search_fields = ('name',)
+    inlines = [AccountTransactionHistoryInLine]
 
 @admin.register(Withdraw)
 class WithdrawAdmin(admin.ModelAdmin):
@@ -18,21 +34,21 @@ class AccountTransferAdmin(admin.ModelAdmin):
     list_filter = ('from_account', 'to_account', 'transferred_at')
     date_hierarchy = 'transferred_at'
 
-@admin.register(AccountTransactionHistory)
-class AccountTransactionHistoryAdmin(admin.ModelAdmin):
-    list_display = ('account', 'description', 'transaction_type', 'action_type', 'amount', 
-                    'previous_balance', 'new_balance', 'timestamp')
-    list_filter = ('account', 'transaction_type', 'action_type', 'timestamp')
-    search_fields = ('description', 'account__name')
-    date_hierarchy = 'timestamp'
-    readonly_fields = ('account', 'amount', 'previous_balance', 'new_balance', 
-                       'transaction_type', 'action_type', 'content_type', 
-                       'object_id', 'description', 'timestamp')
+# @admin.register(AccountTransactionHistory)
+# class AccountTransactionHistoryAdmin(admin.ModelAdmin):
+#     list_display = ('account', 'description', 'transaction_type', 'action_type', 'amount', 
+#                     'previous_balance', 'new_balance', 'timestamp')
+#     list_filter = ('account', 'transaction_type', 'action_type', 'timestamp')
+#     search_fields = ('description', 'account__name')
+#     date_hierarchy = 'timestamp'
+#     readonly_fields = ('account', 'amount', 'previous_balance', 'new_balance', 
+#                        'transaction_type', 'action_type', 'content_type', 
+#                        'object_id', 'description', 'timestamp')
     
-    def has_add_permission(self, request):
-        # Prevent manual creation of history entries
-        return False
+#     def has_add_permission(self, request):
+#         # Prevent manual creation of history entries
+#         return False
     
-    def has_change_permission(self, request, obj=None):
-        # Prevent editing of history entries
-        return False
+#     def has_change_permission(self, request, obj=None):
+#         # Prevent editing of history entries
+#         return False
