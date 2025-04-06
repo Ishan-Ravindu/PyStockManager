d0fe789e54cc7d409949d8eddb67d482e6169d7c
Ishan-Ravindu <ishanravindu27596@gmail.com>
2025-04-06 09:05:39 +0000

    feat: added category for products

--- entity/admin.py
+++ entity/admin.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-from entity.models import Customer, Product, Shop, Supplier
+from entity.models import Category, Customer, Product, Shop, Supplier
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
@@ -33,9 +35,84 @@ class CustomerAdmin(admin.ModelAdmin):
         }),
     )
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'profit_margin')
     search_fields = ('name', 'description')
     list_filter = ('profit_margin',)
+    list_per_page = 20  
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        categories = Category.objects.all()
+        categories_profit_margins = {}
+        for category in categories:
+            categories_profit_margins[category.id] = str(category.profit_margin)
+        js_code = """
+        <script>
+        var categoryProfitMargins = {};
+        """
+        for cat_id, margin in categories_profit_margins.items():
+            js_code += f"categoryProfitMargins[{cat_id}] = '{margin}';\n"
+        js_code += """
+        // Function to update profit margin when category changes
+        function handleCategoryChange() {
+            var allSelects = document.querySelectorAll('select');
+            var categorySelect = null;
+            for (var i = 0; i < allSelects.length; i++) {
+                if (allSelects[i].name && allSelects[i].name.includes('category')) {
+                    categorySelect = allSelects[i];
+                    break;
+                }
+            }            
+            if (!categorySelect) return;
+            var categoryId = categorySelect.value;
+            var allInputs = document.querySelectorAll('input');
+            var profitMarginInput = null;            
+            for (var i = 0; i < allInputs.length; i++) {
+                if (allInputs[i].name && allInputs[i].name.includes('profit_margin')) {
+                    profitMarginInput = allInputs[i];
+                    break;
+                }
+            }            
+            if (!profitMarginInput) return;
+            if (categoryId && categoryProfitMargins[categoryId]) {
+                profitMarginInput.value = categoryProfitMargins[categoryId];
+                profitMarginInput.style.backgroundColor = '#ffffcc';
+                setTimeout(function() {
+                    profitMarginInput.style.backgroundColor = '';
+                }, 1000);
+            }
+        }
+        window.addEventListener('load', function() {
+            var allSelects = document.querySelectorAll('select');            
+            for (var i = 0; i < allSelects.length; i++) {
+                if (allSelects[i].name && allSelects[i].name.includes('category')) {
+                    allSelects[i].addEventListener('change', handleCategoryChange);
+                    allSelects[i].setAttribute('onchange', 'handleCategoryChange()');
+                }
+            }
+        });
+        </script>
+        """
+        self.fields['category'].help_text = mark_safe(
+            "<strong></strong>" + js_code
+        )
+        self.fields['profit_margin'].help_text = mark_safe(
+            "<strong style='color: #336699;'>Add a manual profit margin if you want to use a product-specific profit margin</strong>"
+        )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+    list_display = ('name', 'category', 'description', 'profit_margin')
+    search_fields = ('name', 'description')
+    fields = ('name', 'description', 'category', 'profit_margin')
     list_per_page = 20
--- entity/models.py
+++ entity/models.py
@@ -81,10 +81,22 @@ class Customer(models.Model):
         return format_html('{}<br>{}'.format(credit_status_value, payment_status_value))
     combined_status.short_description = 'Customer Status'
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    profit_margin = models.DecimalField(max_digits=5, decimal_places=2, default=10.00)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     profit_margin = models.DecimalField(max_digits=5, decimal_places=2, default=10.00)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
 
     def __str__(self):
         return f'{self.name}'
