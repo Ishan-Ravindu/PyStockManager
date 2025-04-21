167b41c2c6f9c768b08895d57b9ce3eb6dbbee23
Ishan-Ravindu <ishanravindu27596@gmail.com>
2025-04-21 14:43:30 +0000

    fix: make mobile number optional

--- supplier/models.py
+++ supplier/models.py
@@ -4,7 +4,7 @@ from simple_history.models import HistoricalRecords
 
 class Supplier(models.Model):
     name = models.CharField(max_length=255)
-    mobile_number = models.CharField(validators=[phone_regex], max_length=10)
+    mobile_number = models.CharField(validators=[phone_regex], max_length=10, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
     payable = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
