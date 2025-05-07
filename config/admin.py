d22b8558dfb6e895a2310af28519946e69008482
Ishan-Ravindu <ishanravindu27596@gmail.com>
2025-05-07 19:23:40 +0000

    fix: remove unwanted permistion from admin pnnel

--- config/admin.py
+++ config/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import User, Group
 
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
@@ -17,7 +16,23 @@ class UserAdmin(BaseUserAdmin, ModelAdmin):
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
 
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if 'user_permissions' in form.base_fields:
+            field = form.base_fields['user_permissions']
+            field.queryset = field.queryset.exclude(
+                content_type__model__startswith='historical'
+            )
+        return form
+
 
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
-    pass
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if 'permissions' in form.base_fields:
+            field = form.base_fields['permissions']
+            field.queryset = field.queryset.exclude(
+                content_type__model__startswith='historical'
+            )
+        return form
